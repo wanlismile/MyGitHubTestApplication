c02fcccfc4979bc6ba3812ba4df26b1b7d9dcefc
wanli_smile <wanli_smile@126.com>
2017-12-11 23:06:26 +0800

    master添加

--- MyApplication/app/src/main/java/com/example/wanli_smile/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/wanli_smile/myapplication/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    /**master添加一个a*/
+    int a = 10;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
